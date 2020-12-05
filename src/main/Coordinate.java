0ea107c816b985dd6e83dc5c47d8e2fc6b4b4e3a
BigSalmon1 <brandonchickin@gmail.com>
2020-12-04 19:01:36 -0800

    Made Coordinate.java and its constructor

---
+++ src/main/Coordinate.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Coordinate {
+
+    public double xLoc, yLoc;
+
+    public Coordinate(double xLoc, double yLoc) {
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
+
+    }
+
+}
