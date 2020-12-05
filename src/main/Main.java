396dcc2e23ab18b6a7003b41bd24253f79e1af11
BigSalmon1 <brandonchickin@gmail.com>
2020-12-05 14:05:07 -0800

    Organized classes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import main.coordinatePlane.Coordinate;
+import main.coordinatePlane.LineSegment;
+
 public class Main {
 
     public static void main(String[] args) {
--- src/main/coordinatePlane/LineSegment.java
+++ src/main/coordinatePlane/LineSegment.java
@@ -1,4 +1,4 @@
-package main;
+package main.coordinatePlane;
 
 public class LineSegment {
 
