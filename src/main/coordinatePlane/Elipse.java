766d3ceca2906704e40fc73e2229d89beaa04284
BigSalmon1 <brandonchickin@gmail.com>
2020-12-05 14:08:45 -0800

    Create Elipse.java

---
+++ src/main/coordinatePlane/Elipse.java
@@ -0,0 +1,15 @@
+package main.coordinatePlane;
+
+public class Elipse {
+
+    private Coordinate center;
+    private double height, width;
+
+    public Elipse(Coordinate center, double height, double width) {
+        this.center = new Coordinate(center);
+        this.height = height;
+        this.width = width;
+
+    }
+
+}
