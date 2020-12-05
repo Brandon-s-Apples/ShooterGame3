b2156bdf7a5b2e755d6207cef086cfa83b8b09a3
BigSalmon1 <brandonchickin@gmail.com>
2020-12-04 19:03:54 -0800

    Update Coordinate.java

--- src/main/Coordinate.java
+++ src/main/Coordinate.java
@@ -10,4 +10,22 @@ public class Coordinate {
 
     }
 
+    public void moveXY(double xDiff, double yDiff) {
+        xLoc += xDiff;
+        yLoc += yDiff;
+
+    }
+
+    public void setXY(double xLoc, double yLoc) {
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
+
+    }
+
+    public double getX() {return xLoc;}
+    public double getY() {return yLoc;}
+
+    public int getIntX() {return (int)xLoc;}
+    public int getIntY() {return (int)yLoc;}
+
 }
