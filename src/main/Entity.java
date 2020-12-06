7a952c7a5872cf4c2c3ebae0bc05eb04b531f1e6
Apple123_ <brandontthan2005@gmail.com>
2020-12-06 11:14:38 -0800

    Added Entity.java without testing

---
+++ src/main/Entity.java
@@ -0,0 +1,32 @@
+package main;
+
+import main.coordinatePlane.Coordinate;
+import main.coordinatePlane.Velocity;
+
+import javax.swing.*;
+
+public abstract class Entity extends JLabel {
+
+    public static final int width = 250, height = 250;
+
+    private Coordinate location;
+    private Velocity velocity;
+
+    public Entity(Coordinate location) {
+        this.location = new Coordinate(location);
+
+    }
+
+    abstract void update();
+
+    public void move() {
+        location.move(velocity);
+
+    }
+
+    public void setBounds() {
+        setBounds(location.getIntX() + (Main.windowWidth / 2), -location.getIntY() + (Main.windowHeight / 2), width, height);
+
+    }
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,8 @@ import main.coordinatePlane.*;
 
 public class Main {
 
+    public static final int windowWidth = 1900, windowHeight = 1100;
+
     public static void main(String[] args) {
         Elipse elipse = new Elipse(new Coordinate(0, 0), 5, 10);
         System.out.println(elipse.contains(new Coordinate(0, 0)));
