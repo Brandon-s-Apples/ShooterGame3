f5323e8200af1bf71c55db58214f1d00900cbbad
Apple123_ <brandontthan2005@gmail.com>
2020-12-06 12:29:32 -0800

    Tested maxSpeed with positive results

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,12 +11,18 @@ public class Main {
     public static final int windowWidth = 1900, windowHeight = 1100;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Testing");
+        /*JFrame frame = new JFrame("Testing");
         frame.setSize(windowWidth, windowHeight);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(null);
-        frame.setVisible(true);
+        frame.setVisible(true);*/
+
+        Velocity velocity = new Velocity(45, 100);
+        velocity.setMaxSpeed(150);
+        System.out.println(velocity);
+        velocity.addVelocity(new Velocity(225, 1000));
+        System.out.println(velocity);
 
     }
 
