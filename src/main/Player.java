3a6184c4e31f2a47db498e529d16de118f28e87b
Apple123_ <brandontthan2005@gmail.com>
2020-12-10 22:36:57 -0800

    Implemented Velocity

--- src/main/Player.java
+++ src/main/Player.java
@@ -6,19 +6,23 @@ public class Player extends BLabel {
 
     private ImageIcon appleImageIcon = new ImageIcon("D:/Pictures/apple.png");
     private Coordinate playerLoc;
+    private Velocity playerVel;
 
     public Player() {
         setIcon(appleImageIcon);
         setBounds(0, 0, appleImageIcon.getIconWidth(), appleImageIcon.getIconHeight());
         playerLoc = new Coordinate(0, 0);
+        playerVel = new Velocity(0, 0, 10);
 
     }
 
     public void update() {
-        if(Keyboard.getKey(Keyboard.w)) playerLoc.moveAD(0, 10);
-        if(Keyboard.getKey(Keyboard.a)) playerLoc.moveAD(270, 10);
-        if(Keyboard.getKey(Keyboard.s)) playerLoc.moveAD(180, 10);
-        if(Keyboard.getKey(Keyboard.d)) playerLoc.moveAD(90, 10);
+        if(Keyboard.getKey(Keyboard.w)) playerVel.addAD(0, 1);
+        if(Keyboard.getKey(Keyboard.a)) playerVel.addAD(270, 1);
+        if(Keyboard.getKey(Keyboard.s)) playerVel.addAD(180, 1);
+        if(Keyboard.getKey(Keyboard.d)) playerVel.addAD(90, 1);
+
+        playerLoc.moveV(playerVel);
 
     }
 
