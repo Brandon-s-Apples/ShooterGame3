0503625c19df4e71b3b0a0dd457a21016b906ebb
Apple123_ <brandontthan2005@gmail.com>
2020-12-10 19:56:26 -0800

    Made frame

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,17 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Shooter Game 3");
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(1900, 1100);
+        frame.setVisible(true);
+
+    }
 
 }
