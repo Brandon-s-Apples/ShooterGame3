0b6639b44cdcb7489f19e052c67b2456748723cd
Apple123_ <brandontthan2005@gmail.com>
2020-12-11 21:08:52 -0800

    Made it work

--- src/main/Coordinate.java
+++ src/main/Coordinate.java
@@ -4,9 +4,15 @@ public class Coordinate {
 
     private double xLoc, yLoc;
 
-    public Coordinate() {xLoc = 0; yLoc = 0;}
+    public Coordinate() {
+        xLoc = 0;
+        yLoc = 0;
+    }
 
-    public Coordinate(double x, double y) {xLoc = x; yLoc = y;}
+    public Coordinate(double x, double y) {
+        xLoc = x;
+        yLoc = y;
+    }
 
     public void moveXY(double xDiff, double yDiff) {
         this.xLoc += xDiff;
@@ -15,8 +21,16 @@ public class Coordinate {
     }
 
     public void moveAD(double angle, double distance) {
-        this.xLoc += distance * Math.sin(Math.toRadians(angle));
-        this.yLoc += distance * Math.cos(Math.toRadians(angle));
+        angle %= 360;
+        if(angle == 0) yLoc += distance;
+        else if(angle == 90 || angle == -270) xLoc += distance;
+        else if(angle == 180 || angle == -180) yLoc -= distance;
+        else if(angle == 270 || angle == -90) xLoc -= distance;
+        else {
+            xLoc += distance * Math.sin(Math.toRadians(angle));
+            yLoc += distance * Math.cos(Math.toRadians(angle));
+
+        }
 
     }
 
