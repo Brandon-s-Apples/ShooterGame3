42bcbf800a78684f1b690b077a32faa164e4d243
Apple123_ <brandontthan2005@gmail.com>
2020-12-21 13:03:52 -0800

    Added player

---
+++ src/game/Player.java
@@ -0,0 +1,19 @@
+package game;
+
+import inputs.Constants;
+import types.code.Coordinate;
+import types.graphics.BLabel;
+
+import javax.swing.*;
+
+public class Player extends BLabel {
+
+    private Coordinate loc;
+
+    public Player(JComponent game) {
+        super(game);
+        loc = new Coordinate(Constants.spawnX, Constants.spawnY);
+
+    }
+
+}
--- src/inputs/Constants.java
+++ src/inputs/Constants.java
@@ -2,11 +2,14 @@ package inputs;
 
 public class Constants {
 
-    // src/main/Main.java
-    public static final int frameWidth = 1900, frameHeight = 1100;
-
-    // src/main/Keyboard.java
+    // src/inputs/Keyboard.java
     public static final int supportedKeys = 4;
     public static final int wKey = 0, aKey = 1, sKey = 2, dKey = 3;
+
+    // src/game/Main.java
+    public static final int frameWidth = 1900, frameHeight = 1100;
+
+    // src/game/Player.java
+    public static final double spawnX = 0, spawnY = 0;
 
 }
