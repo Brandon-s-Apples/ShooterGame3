a330cc822081243ab158ddc569448a4d580cd556
Apple123_ <brandontthan2005@gmail.com>
2020-12-21 10:22:27 -0800

    Create BFrame.java

---
+++ src/main/BFrame.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+
+public class BFrame extends JFrame {
+
+    public BFrame(String title, int width, int height) {
+        setTitle(title);
+        setSize(width, height);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+
+    }
+
+}
