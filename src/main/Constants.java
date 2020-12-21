c00a54fa9335a92fab54493e868aa38221a35295
Apple123_ <brandontthan2005@gmail.com>
2020-12-21 10:39:45 -0800

    Tested

---
+++ src/main/Constants.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Constants {
+
+    // src/main/Main.java
+    public static final int frameWidth = 1900, frameHeight = 1100;
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        BFrame frame = new BFrame("Shooter Game 3", Constants.frameWidth, Constants.frameHeight);
+        frame.setVisible(true);
+
     }
 
 }
