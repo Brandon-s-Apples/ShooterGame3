e54bb852ea8b3f89332aa5b05d79d53e3b75c831
Apple123_ <brandontthan2005@gmail.com>
2020-12-23 13:44:53 -0800

    added player and enemyList references to Bullet

--- src/game/entity/bullet/Bullet.java
+++ src/game/entity/bullet/Bullet.java
@@ -1,12 +1,17 @@
 package game.entity.bullet;
 
 import game.entity.Entity;
+import game.entity.enemy.EnemyList;
+import game.entity.player.Player;
 import inputs.Constants;
 import types.code.*;
 
 import javax.swing.*;
 
 public class Bullet extends Entity {
+
+    private static EnemyList enemyListRef;
+    private static Player playerListRef;
 
     public Bullet(BulletList bulletList, Coordinate startLoc, Velocity velocity) {
         super(bulletList);
@@ -20,11 +25,21 @@ public class Bullet extends Entity {
     }
 
     protected void move(Coordinate loc, Velocity vel) {
+        Coordinate startLoc = new Coordinate(getLoc());
         loc.moveV(vel);
+        Coordinate endLoc = new Coordinate(getLoc());
     }
 
     public void update() {
         super.update(Constants.origin);
+    }
+
+    public static void setEnemyListRef(EnemyList enemyListRef) {
+        Bullet.enemyListRef = enemyListRef;
+    }
+
+    public static void setPlayerListRef(Player playerListRef) {
+        Bullet.playerListRef = playerListRef;
     }
 
 }
