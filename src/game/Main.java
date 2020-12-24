325b8d2d0cfd78a98576b1df21714b830dc9cfaa
Apple123_ <brandontthan2005@gmail.com>
2020-12-23 20:01:18 -0800

    ...

--- src/game/Main.java
+++ src/game/Main.java
@@ -14,12 +14,18 @@ public class Main {
 
         // frame.setVisible(true);
 
+        Coordinate co = new Coordinate(1, 7.8);
         Ellipse el = new Ellipse(new Coordinate(7.44 ,4.57), 17.68, 10.32);
-        LineSegment ls = new LineSegment(new Coordinate(0, 7.8), new Coordinate(15, 7.8));
+        LineSegment ls = new LineSegment(new Coordinate(0, 7.8), new Coordinate(7.8, 0));
 
         Coordinate[] a = BMath.intersects(ls, el);
         if(a.length > 0) System.out.println(a[0]);
         if(a.length > 1) System.out.println(a[1]);
+
+        System.out.println(!BMath.contains(co, ls));
+
+        co = new Coordinate(BMath.intersects(ls, el)[0]);
+        System.out.println(co);
 
         // new Game(frame);
 
